85c14e2e61bc01c6db016bdd6967ac172aad4240
HECTOR CHAPA <hchapa@lrgvdc911.org>
2019-06-14 13:37:49 -0500

    display only the map based on click by user

--- src/pages/DashBoardCard.svelte
+++ src/pages/DashBoardCard.svelte
@@ -3,24 +3,33 @@
     import { fade, fly } from 'svelte/transition';
     import EsriMap from '../ui/EsriMap.svelte';
 
-    
     export let url;
     export let path;
     let Tickets = [];
     let ticketCount = 0;
-    let mapAvailable = true;
+    let mapAvailable = false;
      onMount(async () => {
         
         const res = await fetch(url + path);
         
         let hold = await res.json();
         Tickets = (hold) ? hold['data'] : [];
+        console.log(Tickets);
+        
+        
         ticketCount = Tickets.length;
         
      });
 
     function MapError() {
         mapAvailable = false;
+    }
+
+    function test(index) {
+        console.log("AM RUNNING");
+        
+        console.log(Tickets[index]);
+        Tickets[index].MapView = !Tickets[index].MapView;    
     }
 
 </script>
@@ -37,13 +46,18 @@
         margin-top: 0 !important;
     }
 
+    img {
+        max-width: 100%;
+        max-height: 100%;
+    }
+
 </style>
  <button out:fade style="position: fixed; bottom: 10px; right: 10px;" class="action-button rotate-minus bg-red fg-white">
     <span style="font-size: 18px; font-weight: bold;">{ticketCount}</span>
 </button>
 <div class="contanier-fluid gridDash">
     
-    {#each Tickets as ticket}
+    {#each Tickets as ticket, i}
          <div in:fly="{{ y: 200, duration: 2000 }}" out:fade class="card">
             <div class="card-header">
                 <div class="avatar">
@@ -52,15 +66,15 @@
                 <div class="name">{ticket.cfirst_name + " " + ticket.clast_name}</div>
                 <div class="date">{ticket.created_date}</div>
             </div>
-            <div class="card-content">
-                {#if mapAvailable}
+            <div on:click={() => {test(i)}} class:p-2={!ticket.MapView} class="card-content">
+            <input type=checkbox bind:checked={ticket.MapView}>
+                {#if ticket.MapView}
                      <!-- content here -->
-                     <EsriMap on:MapError={MapError} pictureURLMarker={"../assets/PurpleShinyPin.png"} center={[ ticket.longy,ticket.lat]} idTicket={ticket.id_ticket} />
+                     <EsriMap on:MapError={MapError} pictureURLMarker={"../../assets/PurpleShinyPin.png"} center={[ ticket.longy,ticket.lat]} idTicket={ticket.id_ticket} />
                 {:else}
                      <!-- else content here -->
-                     <img src="../assets/globe-map-icon.webp" alt="Map Preview">
+                     <img  src="../../assets/globe-map-icon.webp" alt="Map Preview">
                 {/if}
-                
                 
             </div>
             <div class="card-footer">
